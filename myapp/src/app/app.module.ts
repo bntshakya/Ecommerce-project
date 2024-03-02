cdfa201e1a7cf2fb74ed105c2fdc01046ea9b8ea
bntshakya <bntshakya@gmail.com>
2024-03-02 20:33:37 +0545

    add: register and add details to mockapi

--- myapp/src/app/app.module.ts
+++ myapp/src/app/app.module.ts
@@ -8,17 +8,14 @@ import { LoginComponent } from './login/login.component';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatSelectModule } from '@angular/material/select';
 import { MatInputModule } from '@angular/material/input';
-import {MatButtonModule} from '@angular/material/button';
-import { ReactiveFormsModule } from '@angular/forms';import { NavbarComponent } from './navbar/navbar.component';
+import { MatButtonModule } from '@angular/material/button';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { NavbarComponent } from './navbar/navbar.component';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { HttpClientModule } from '@angular/common/http';
-
+import { MatCheckboxModule } from '@angular/material/checkbox';
 @NgModule({
-  declarations: [
-    AppComponent,
-    LoginComponent,
-    NavbarComponent
-    ],
+  declarations: [AppComponent, LoginComponent, NavbarComponent],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -27,9 +24,11 @@ import { HttpClientModule } from '@angular/common/http';
     MatSelectModule,
     MatInputModule,
     MatButtonModule,
+    FormsModule,
     ReactiveFormsModule,
     MatToolbarModule,
-    HttpClientModule
+    HttpClientModule,
+    MatCheckboxModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- myapp/src/app/login/login.component.ts
+++ myapp/src/app/login/login.component.ts
@@ -1,5 +1,10 @@
-import { Component } from '@angular/core';
-import { FormBuilder, Validators } from '@angular/forms';
+import { Component, OnInit } from '@angular/core';
+import {
+  AbstractControl,
+  FormBuilder,
+  FormGroup,
+  Validators,
+} from '@angular/forms';
 import { AuthService } from '../auth.service';
 
 @Component({
@@ -7,23 +12,43 @@ import { AuthService } from '../auth.service';
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css'],
 })
-  
-export class LoginComponent {
-  constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
-  
+export class LoginComponent implements OnInit {
+  public profileForm!: FormGroup;
+  // public ValidatePassword!: any;
 
-  profileForm = this.formBuilder.group({
-    firstName: ['', Validators.required],
-    lastName: ['', Validators.required],
-    email: [
-      '',
-      [
-        Validators.required,
-        Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+  constructor(private formBuilder: FormBuilder, private auth: AuthService) {}
+  ngOnInit(): void {
+    this.profileForm = this.formBuilder.group({
+      firstName: ['', Validators.required],
+      lastName: ['', Validators.required],
+      email: [
+        '',
+        [
+          Validators.required,
+          Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,4}$'),
+        ],
       ],
-    ],
-    password: ['', [Validators.required, Validators.minLength(8)]],
-  });
+      password: ['', [Validators.required, Validators.minLength(8)]],
+      confirm_password: [
+        '',
+        [Validators.required, Validators.minLength(8),this.validatePassword],
+      ],
+      terms: [false, Validators.requiredTrue],
+    });
+  }
+  validatePassword(control: AbstractControl): { [key: string]: any } | null {
+    const confirmPassword = control.value;
+    const passwordControl = control.root.get('password');
+    if (!passwordControl) {
+      return null; 
+    }
+    const password = passwordControl.value;
+
+    if (confirmPassword !== password) {
+      return { passwordMismatch: 'Passwords do not match'};
+    }
+    return null;
+  }
 
   onSubmit() {
     this.auth.register(
@@ -32,5 +57,6 @@ export class LoginComponent {
       this.profileForm.value.email,
       this.profileForm.value.password
     );
+    this.profileForm.reset();
   }
 }
