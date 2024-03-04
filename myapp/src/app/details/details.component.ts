aa3781e24f218029260829f4be7c888c4ba781aa
bntshakya <bntshakya@gmail.com>
2024-03-04 22:42:59 +0545

    add : details page skeleton

--- myapp/src/app/details/details.component.ts
+++ myapp/src/app/details/details.component.ts
@@ -1,10 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { UserService } from '../user.service';
 
 @Component({
   selector: 'app-details',
   templateUrl: './details.component.html',
-  styleUrls: ['./details.component.css']
+  styleUrls: ['./details.component.css'],
 })
-export class DetailsComponent {
+export class DetailsComponent implements OnInit {
+  id?: number;
+  private sub: any;
+  constructor(
+    private route: ActivatedRoute,
+    private userservice: UserService
+  ) {}
+  ngOnInit(): void {
+    this.sub = this.route.params.subscribe((params) => {
+      this.id = +params['id'];
+    });
+   this.getdetails(); 
+  }
+  
 
+  getdetails() {
+    const product_info = this.userservice.getProducts(
+      `https://fakestoreapi.com/products/${this.id}`
+    ).subscribe((data)=>{
+      console.log(data);
+    });
+    // console.log(product_info, 'a');
+  }
+
+  
 }
--- myapp/src/app/user.service.ts
+++ myapp/src/app/user.service.ts
@@ -7,8 +7,7 @@ import { Observable } from 'rxjs';
 export class UserService {
   constructor(private http: HttpClient) {}
 
-  public getProducts(): Observable<any> {
-    const url = 'https://fakestoreapi.com/products/category/electronics';
+  public getProducts(url = 'https://fakestoreapi.com/products/category/electronics'): Observable<any> {
     console.log('at user service');
     return this.http.get<any>(url);
   }
