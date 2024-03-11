0b6561aedc6fa12149221819b2097b9ab8628340
bntshakya <bntshakya@gmail.com>
2024-03-11 17:42:08 +0545

    feat: add search component with query parameters

--- myapp/src/app/app-routing.module.ts
+++ myapp/src/app/app-routing.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomepageComponent } from './homepage/homepage.component';
 import { DetailsComponent } from './details/details.component';
+import { SearchComponent } from './search/search.component';
+import { LoginComponent } from './login/login.component';
 
 const routes: Routes = [
   { path: 'homepage', component: HomepageComponent },
   { path: '', redirectTo: '/homepage', pathMatch: 'full' },
-  { path: 'homepage/:id', component: DetailsComponent}
+  { path: 'homepage/:id', component: DetailsComponent },
+  { path: 'search', component: SearchComponent },
+  { path: 'login', component: LoginComponent },
 ];
 
 @NgModule({
--- myapp/src/app/search/search.component.ts
+++ myapp/src/app/search/search.component.ts
@@ -1,10 +1,54 @@
-import { Component } from '@angular/core';
+import { group } from '@angular/animations';
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MatCheckbox } from '@angular/material/checkbox';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-search',
   templateUrl: './search.component.html',
-  styleUrls: ['./search.component.css']
+  styleUrls: ['./search.component.css'],
 })
-export class SearchComponent {
+export class SearchComponent implements OnInit {
+  constructor(
+    private formbuilder: FormBuilder,
+    private router: Router,
+    private route: ActivatedRoute
+  ) {}
+  public filterparams!: FormGroup;
 
+  ngOnInit(): void {
+    this.filterparams = this.formbuilder.group({
+      // price : this.formbuilder.group({
+      minprice: ['', Validators.pattern('^[0-9]*$')],
+      maxprice: ['', Validators.pattern('^[0-9]*$')],
+      // }),
+      mclothing: [true],
+      fclothing: [true],
+      electronics: [true],
+      jewelery: [true],
+    });
+  }
+
+  onsubmit() {
+    // console.log('asdfasf');
+    const selectedCategories: string[] = [];
+    if (this.filterparams.value.mclothing)
+      selectedCategories.push('maleclothing');
+    if (this.filterparams.value.fclothing)
+      selectedCategories.push('femaleclothing');
+    if (this.filterparams.value.electronics)
+      selectedCategories.push('electronics');
+    if (this.filterparams.value.jewelery) selectedCategories.push('jewelery');
+    const queryParams = {
+      minprice: this.filterparams.value.minprice,
+      maxprice: this.filterparams.value.maxprice,
+      products: selectedCategories.join(','),
+    };
+
+    this.router.navigate(['.'], {
+      relativeTo: this.route,
+      queryParams: queryParams,
+    });
+  }
 }
