e5e35a4e2089fc227267a0151a4fd76ab0a81878
bntshakya <bntshakya@gmail.com>
2024-03-11 12:08:18 +0545

    refactor: moved card rendering from card component to home component
    This commit moves the card rendering from card to home. The card
    component now displays a single card and receives the details from home
    component

--- myapp/src/app/homepage/homepage.component.ts
+++ myapp/src/app/homepage/homepage.component.ts
@@ -1,19 +1,38 @@
 import { Component, OnInit } from '@angular/core';
-// import { UserService } from '../user.service';
+import { UserService } from '../user.service';
 import { ProductarrayService } from '../productarray.service';
 @Component({
   selector: 'app-homepage',
   templateUrl: './homepage.component.html',
   styleUrls: ['./homepage.component.css'],
 })
-export class HomepageComponent {
-  constructor() { // public productarrayservice: ProductarrayService // public userService: UserService,
+export class HomepageComponent implements OnInit{
+  constructor(public userservice:UserService) { // public productarrayservice: ProductarrayService // public userService: UserService,
     console.log('at homepage constructor');
   }
+  product_array: any[] = [];
   // ngOnInit(): void {
   //   this.userService.getProducts().subscribe((data) => {
   //     this.productarrayservice.product_array = data;
   //     console.log(this.productarrayservice.product_array);
   //   });
   // }
+
+  ngOnInit(): void {
+    // console.log('at card oninit');
+    // console.log(this.product_array, 'av');
+    this.userservice.getProducts().subscribe((data) => {
+      this.product_array = data;
+      // this.product_array = this.productarrayservice.product_array;
+      // console.log(this.product_array);
+      // console.log('at card oninit');
+      // this.product_name = this.product_array[0].title;
+      // this.product_price = this.product_array[0].price;
+      // this.product_description =
+      //   this.product_array[0].description;
+      // this.product_photo = this.product_array[0].image;
+      // this.product_category =
+      //   this.product_array[0].category;
+    });
+  }
 }
