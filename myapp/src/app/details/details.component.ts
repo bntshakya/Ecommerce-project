9a886bb84b92edf07fdb210bacc02b599e294fc7
bntshakya <bntshakya@gmail.com>
2024-03-13 12:12:55 +0545

    refactor:add access modifiers

--- myapp/src/app/details/details.component.ts
+++ myapp/src/app/details/details.component.ts
@@ -10,31 +10,23 @@ import { UserService } from '../user.service';
 export class DetailsComponent implements OnInit {
   id?: number;
   private sub: any;
-  // public src: string = '';
   public product_object : any ;
   constructor(
     private route: ActivatedRoute,
     private userservice: UserService
   ) {}
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.sub = this.route.params.subscribe((params) => {
       this.id = +params['id'];
     });
     this.getdetails();
   }
   
-
-  getdetails() {
+  public getdetails(): void {
     this.userservice.getProducts(
       `https://fakestoreapi.com/products/${this.id}`
     ).subscribe((data)=>{
-      // console.log(data);
       this.product_object = data;
-      // this.src = this.product_object?.image;
-      // console.log(this.product_objectf);
     });
-    // console.log(product_info, 'a');
   }
-
-  
 }
